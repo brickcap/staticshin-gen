c26a726747496940b9574c01399c45625df9d1be
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-04-14 22:20:30 +0530

    About added with generate

--- generate.js
+++ generate.js
@@ -3,6 +3,11 @@ var consolidate = require('consolidate');
 var mustache = require('mustache');
 var fs = require('fs-extra');
 
+var renderString = fs.readFileSync("/home/akshat/Repo/staticshin/about.html");
+var header = fs.readFileSync("/home/akshat/Repo/staticshin-gen/views/header.html");
+var rendered = mustache.render(renderString.toString(),{head:header});
+fs.outputFileSync('/home/akshat/Repo/staticshin/about.html',rendered);
+
 request("http://localhost:9200/blog/_search?size=79",function(error,response,body){
     var parsed = JSON.parse(body);
     var hits = parsed.hits.hits;
@@ -22,3 +27,4 @@ request("http://localhost:9200/blog/_search?size=79",function(error,response,bod
 	
     }
 });
+
