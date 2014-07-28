f0e3c11a7b11969ecd6eb01c879c5ab1c319c2e6
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-07-29 00:24:02 +0530

    Import to couchdb complete

--- couchdb/importer.js
+++ couchdb/importer.js
@@ -1,11 +1,20 @@
 var request = require("request");
+var _ = require("underscore");
 
 var importer = function(){
-//reading all data from elastic search
+    //reading all data from elastic search
 
+    request("http://localhost:9200/blog/_search?size=100",function(error,response,data){
+	var parsed = JSON.parse(data);
+	var hits = _.pluck(parsed.hits.hits,"_source");
+	//pushing it to couch :)
+	request.post("http://localhost:5984/staticshin/_bulk_docs",
+		     {json:{"docs":hits}},
+		     function(error,response,data){
+			 console.log(data);
+		     });
+    });
 
-
-//pusing all data to couchdb
 };
 
 importer();
