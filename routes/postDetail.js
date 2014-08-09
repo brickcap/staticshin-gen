3c116fc577e52177b8c9a4e860d207eb2de47048
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-08-10 00:32:02 +0530

    Moved the generation code to postDetail

--- routes/postDetail.js
+++ routes/postDetail.js
@@ -5,26 +5,26 @@ var fs = require("fs-extra");
 var whitelist = require("../preferences").whitelist;
 
 exports.postDetail = function(req,res){
-  
+    
     var id = req.params.id;
     var url = constants.queries.postType()+id;
     request(url,function(error,response,body){
         try{
-        var parsed = JSON.parse(body);      
-        parsed._source.postedOn = new Date(parsed._source.postedOn).toDateString();
+            var parsed = JSON.parse(body);      
+            parsed._source.postedOn = new Date(parsed._source.postedOn).toDateString();
 	    var temp =  fs.readFileSync('views/post_template.html');
 	    var header = fs.readFileSync('views/header.html');
 	    parsed.header = header;
 	    var render =mustache.render(temp.toString(),parsed);
 	    if(parsed.hasOwnProperty("tags")){
-	    
-	    parsed.tags.forEach(function(element,index,arr){
-		if(whitelist.indexOf(element)!=-1){
-		    fs.outputFileSync("/home/akshat/Repo/staticshin/"+element.toString()+"/index.html",render);
+		
+		parsed.tags.forEach(function(element,index,arr){
+		    if(whitelist.indexOf(element)!=-1){
+			fs.outputFileSync("/home/akshat/Repo/staticshin/"+element.toString()+"/index.html",render);
 
-		}
-	    });
-	}
+		    }
+		});
+	    }
 	    fs.outputFileSync('/home/akshat/Repo/staticshin/'+parsed._id+'/index.html',render);
 	    
 	    return res.render(constants.views.postDetail,parsed);
