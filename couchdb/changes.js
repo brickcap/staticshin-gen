b3a54caaf9cc2fbfb73fbdb302c4c7a36b7d624f
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-08-09 19:32:10 +0530

    Tag directory checks for the whitelist of tags->directory mapping

--- couchdb/changes.js
+++ couchdb/changes.js
@@ -1,9 +1,11 @@
 var request = require("request");
-
+var tagDir = ["programming"];
 
 //start listening to couchdb changes and run scripts on them
 
-var staticshinLatest = "http://ABBA:danging-queen@localhost:5984/staticshin?descending=true&limit=1&include_docs=true";
+console.log("Listening to couchdb changes....");
+var staticshinLatest = "http://ABBA:danging-queen@localhost:5984/test?descending=true&limit=1&include_docs=true";
 
 request(staticshinLatest,function(error,reponse,body){
+    console.log(body);
 });
