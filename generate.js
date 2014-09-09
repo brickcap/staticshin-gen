0d6e3771ec84f8157e99c44e432093c40f73de58
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-09-09 15:49:27 +0530

    Rendered the about page correctly

--- generate.js
+++ generate.js
@@ -3,16 +3,11 @@ var consolidate = require('consolidate');
 var mustache = require('mustache');
 var fs = require('fs-extra');
 var whitelist = require("./preferences").preferences.whitelist;
-
-var renderString = fs.readFileSync("/home/akshat/Repo/staticshin/about.html");
-var header = fs.readFileSync("/home/akshat/Repo/staticshin-gen/views/header.html");
-var rendered = mustache.render(renderString.toString(),{head:header});
-
 var argument = process.argv[2];
 var url = argument?"http://localhost:9200/blog/_search?pretty=true&q=tags:"+argument:
     "http://localhost:9200/blog/_search?size=100000";
 
-fs.outputFileSync('/home/akshat/Repo/staticshin/about.html',rendered);
+renderAbout();
 
 request(url,function(error,response,body){
     console.log(argument);
@@ -23,13 +18,13 @@ request(url,function(error,response,body){
     for(i;i<hits.length;i++){
 	var hit = hits[i];
 	var id = hit._id;
-	var untaggedCriteria = 
 	hit._source.postedOn = new Date(hit._source.postedOn).toDateString();	
 	var temp =  fs.readFileSync('views/post_template.html');
 	//console.log(hit._source);
 	var header = fs.readFileSync('views/header.html');
 	hit.header = header;
 	var tags = hit._source.tags;
+	var untaggedCriteria = tags&&tags.length===0&&!argument||!tags;
 	//First handle the rendering of tagged files
 	if(tags&&tags.length>0){
 	     tags.forEach(function(tag){
@@ -40,14 +35,19 @@ request(url,function(error,response,body){
 		     fs.outputFileSync(whitelist[tag]+id+'/index.html',render);
 		 }
 		 if(!whitelist.hasOwnProperty(tag)){
+		     console.log("rendering tagged blacklisted post "+i.toString());
 		     fs.outputFileSync('/home/akshat/Repo/staticshin/'+id+'/index.html',render);
 
 		 }
 	    });
 	}
 	// okay now no more tags are left. Render away
-	if(tags&&tags.length===0&&!argument){
-	    console.log("renreding untagged post "+i.toString());
+	
+	//if there are tags the length must be zero other wise tags can be undefined. 
+	//There should be no arguments supplied by the command line as well
+
+	if(untaggedCriteria){
+	    console.log("rendering untagged post "+i.toString());
 	    var render =mustache.render(temp.toString(),hit);
 	    fs.outputFileSync('/home/akshat/Repo/staticshin/'+id+'/index.html',render);
 	}
@@ -55,3 +55,11 @@ request(url,function(error,response,body){
     }
 });
 
+
+function renderAbout(){
+var aboutRenderString = fs.readFileSync("/home/akshat/Repo/staticshin/about.html");
+var header = fs.readFileSync("/home/akshat/Repo/staticshin-gen/views/header.html");
+var rendered = mustache.render(aboutRenderString.toString(),{header:header});
+fs.outputFileSync('/home/akshat/Repo/staticshin/about.html',rendered);
+console.log("about page rendered");
+}
