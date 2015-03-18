623c826bbf9c4218d8e1956ae1026847e5a7f802
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2015-03-18 23:47:44 +0530

    MOdified the template to support the rss feed

--- core/archives.js
+++ core/archives.js
@@ -8,9 +8,9 @@ var _ = require("underscore");
 var blacklist = ["wrinq"];
 
 exports.getArchives = function(req,res,api){
-    request.get("/feed_tagged/rss");
-    request.get("/feed/rss");
-    request.get("feed/atom");
+    request.get("http://localhost:5000/feed_tagged/rss");
+    request.get("http://localhost:5000/feed/rss");
+    request.get("http://localhost:5000/feed/atom");
     request(buildArchivesQuery(),function(error,response,body){
 
 	if(error||!body) res.send(500);
